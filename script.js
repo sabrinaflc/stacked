981ba28762d88e27a948b27eda91090483a16ce8
Sabrina Lins <brina.lins@outlook.com>
2025-04-07 22:51:41 -0300

    cart page config

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ window.addEventListener("load",function(){
     setTimeout(closepreloader, 2000);
     
 });
+
+/* Botão finalizar carrinho */
+$(function () {
+    $("#button").click(function () {
+      var $btn = $(this);
+  
+      // Adiciona a classe de carregamento (rotação)
+      $btn.addClass("onclic");
+  
+      // Após 2.25s, troca para validação
+      setTimeout(function () {
+        $btn.removeClass("onclic");
+        $btn.addClass("validate");
+  
+        // Após 2.25s, volta ao estado original
+        setTimeout(function () {
+          $btn.removeClass("validate");
+        }, 2250);
+      }, 2250);
+    });
+  });
+  
